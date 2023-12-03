a78ed61ab9ed708a3df33faa3e151247e3714aec
abdulazizbek001 <olimjonovabdulazizbek001@gmail.com>
2023-12-03 18:11:16 +0500

    init commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+
+const elBox = document.querySelector('#box');
+const elBoxImg = document.querySelector('#box-img');
+const elBoxTitle = document.querySelector('#box-title');
+const elBoxText = document.querySelector('#box-text');
+const elBoxImg2 = document.querySelector('#box-img2');
+
+// ============================= BOX START ============================= //
+
+elBox.style.width = '263px';
+elBox.style.heigth = '370px';
+elBox.style.paddingTop = '30px';
+elBox.style.borderRadius = '10px';
+elBox.style.backgroundColor = '#262D33';
+
+// ============================= BOX END ============================= //
+
+// ============================= BOX-IMG START ============================= //
+
+elBoxImg.style.marginLeft = '56px';
+elBoxImg.style.marginBottom = '20px';
+
+// ============================= BOX-IMG END ============================= //
+
+// ============================= BOX-TITLE START ============================= //
+
+elBoxTitle.style.fontSize = '15px';
+elBoxTitle.style.color = '#939699';
+elBoxTitle.style.marginBottom = '10px';
+elBoxTitle.style.textAlign = 'center';
+elBoxTitle.style.textTransform = 'uppercase'
+
+// ============================= BOX-TITLE END ============================= //
+
+// ============================= BOX-TEXT START ============================= //
+
+elBoxText.style.width = '160px';
+elBoxText.style.margin = '0 auto';
+elBoxText.style.color = 'white'
+elBoxText.style.fontSize = '16px';
+elBoxText.style.textAlign = 'center';
+elBoxText.style.fontWeight = '900';
+elBoxText.style.fontStyle = 'normal';
+elBoxText.style.marginBottom = '20px';
+
+// ============================= BOX-TEXT END ============================= //
+
+// ============================= BOX-IMG2 START ============================= //
+
+elBoxImg2.style.marginLeft = '122px';
+elBoxImg2.style.marginBottom = '18px';
+
+// ============================= BOX-IMG2 END ============================= //
